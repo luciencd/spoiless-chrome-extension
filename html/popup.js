a460c227a3059f7e10158d870acbbae14d5b8405
lucien christie-dervaux <lucienchristiedervaux@gmail.com>
2016-10-28 19:30:44 -0400

    skeleton functions for angular actions

--- html/popup.js
+++ html/popup.js
@@ -6,11 +6,35 @@ app.controller('MainCtrl',
     $scope.showList = [{"seriesid":1,"seriesName":'GOT'},
     {"seriesid":2,"seriesName":'FAB'},
     {"seriesid":3,"seriesName":'TRU'}];
+    //Need to access this list via a REST API call
+
     $scope.selectedItem = $scope.showList[0].series;
 
     //$scope.selectedShows = ["ASS","LUG"];
+
     $scope.selectedShows = [{"seriesid":1,"seriesName":'GOT'}];
+    //Need to access this list via a REST API call
 
     $scope.test = "test";
   }
 ]);
+//input: none
+//output: JSON list of all {showid's and shownames}
+function getAllShows(){
+
+}
+
+//input seriesid
+//output success or failure
+function addShow(){
+
+}
+function removeShow(){
+
+}
+function hideShow(){
+
+}
+function showShow(){
+
+}
