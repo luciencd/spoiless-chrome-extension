94b143885aa3430c4fa9660faefa4a777be1439f
lucien christie-dervaux <lucienchristiedervaux@gmail.com>
2016-11-20 23:36:15 -0500

    no more delay in adding spoilers no more alerts

--- html/popup.js
+++ html/popup.js
@@ -30,21 +30,43 @@ app.controller('MainCtrl', function($scope, $http) {
       $scope.userSeriesList = response.data.Series;
       console.log(response.data.Series);
     });*/
-    getUserSpoilers($scope.user_id,23123);
+    $scope.userSeriesList = [];
+    $scope.getUserSpoilersCtrl = function(user_id){
+      $scope.userSeriesList = getUserSpoilers(user_id,23123);
+    }
 
-
-    $scope.addUserSpoilerCtrl = function(){
+    //Fix this one to be more like the others.
+    $scope.addUserSpoilerCtrl = function(user_id){
       addUserSpoiler($scope.user_id,$scope.selectedItem.series_id,12312321);
-      getUserSpoilers($scope.user_id,23123);
+      //alert("userlist"+$scope.userSeriesList);
+      console.log($scope.userSeriesList);
+      $scope.userSeriesList = $scope.userSeriesList.concat([{"series_id":$scope.selectedItem.series_id,"seriesName":$scope.selectedItem.seriesName}]);
+      console.log($scope.userSeriesList);
+      //alert("userlist"+$scope.userSeriesList);
+      //getUserSpoilersCtrl($scope.user_id);
       //Edit angularjs to add this series
     }
 
-    $scope.removeUserSpoilerCtrl = function(series_id){
-      alert("ass"+series_id);
+    $scope.removeUserSpoilerCtrl = function(series_id,user_id){
+      //alert("ass"+series_id);
       console.log(series_id);
-      removeUserSpoiler($scope.user_id,series_id,12312321);
-      getUserSpoilers($scope.user_id,23123);
+      removeUserSpoiler(user_id,series_id,12312321);
+      getUserSpoilers(user_id,23123);
       //Edit angularjs to add this series
+    }
+
+    //plain_text
+    $scope.checkSpoilerText = function(){
+      plain_text = "Game of thrones whisper whisper whisper....";
+      alert("checking plain text: '"+plain_text+"' for spoilers.");
+
+    }
+
+    //reddit_comment
+    $scope.checkSpoilerRedditComment = function(){
+      reddit_comment = "<comment><id>1122</id><upvotes>-12</upvotes><text>Someone dies!</text></comment>";
+      alert("checking reddit comment: '"+reddit_comment+"' for spoilers.");
+
     }
 
 
@@ -68,14 +90,14 @@ app.controller('MainCtrl', function($scope, $http) {
     //input seriesid
     //output success or failure
     function addUserSpoiler(user_id,series_id,api_key){
-      alert("addUserSpoiler function ("+user_id+","+series_id+") ");
+      //alert("addUserSpoiler function ("+user_id+","+series_id+") ");
       $http({method: "GET",
             url:"http://localhost:5000/addUserSpoiler",
             params:{"user_id":user_id,"series_id":series_id,"api_key":api_key}
           }).then(function(response) {
 
-              alert("addUserSpoiler return");
-              alert(response);
+              //alert("addUserSpoiler return");
+              //alert(response);
 
 
               console.log(response.data);
@@ -88,9 +110,10 @@ app.controller('MainCtrl', function($scope, $http) {
             params:{"user_id":user_id,"series_id":series_id,"api_key":api_key}
           }).then(function(response) {
 
-              alert("removeUserSpoiler return");
+              //alert("removeUserSpoiler return");
 
-              alert(response);
+              //alert(response);
+              console.log(response.data);
 
           }
       );
@@ -102,8 +125,7 @@ app.controller('MainCtrl', function($scope, $http) {
             params:{"user_id":user_id,"series_id":series_id,"api_key":api_key}
           }).then(function(response) {
 
-              alert("hideUserSpoiler return");
-              alert(response);
+              console.log(response.data);
 
 
           }
@@ -115,8 +137,7 @@ app.controller('MainCtrl', function($scope, $http) {
             params:{"user_id":user_id,"series_id":series_id,"api_key":api_key}
           }).then(function(response) {
 
-              alert("showUserSpoiler return");
-              alert(response);
+              console.log(response.data);
 
 
           }
@@ -128,9 +149,10 @@ app.controller('MainCtrl', function($scope, $http) {
             params:{"user_id":user_id,"api_key":api_key}
           }).then(function(response) {
 
-              alert("getUserSpoiler return");
-              alert(response);
-              $scope.userSeriesList = response.data.Series;
+
+
+              console.log(response.data);
+              return response.data.Series;
           }
       );
     }
